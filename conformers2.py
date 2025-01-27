7f19babdf0b34a7c28c9f4817025f9483f70722e
aaishams <115831912+aaishams@users.noreply.github.com>
2025-01-27 13:00:22 +0530

    Add files via upload
    A new better version of conformers.py

---
+++ conformers2.py
@@ -0,0 +1,170 @@
+import re
+import math
+import os
+
+def extract_coordinates(file_path):
+    coordinates = []
+    with open(file_path, 'r') as file:
+        content = file.readlines()
+    start_pattern = re.compile("Charge =  0 Multiplicity = 1")
+    end_pattern = re.compile("  ")
+    inside_section = False
+    for line in content:
+        if start_pattern.search(line):
+            inside_section = True
+            continue
+        if inside_section and end_pattern.match(line):
+            break
+        if inside_section:
+            c = line.split()
+            if len(c) == 4:
+                atom = str(c[0])
+                x, y, z = float(c[1]), float(c[2]), float(c[3])
+                coordinates.append((atom, x, y, z))
+    return coordinates
+
+def convert_to_xyz(file_path, coordinates):
+    output_file = file_path.replace('.log', '.xyz')
+    with open(output_file, 'w') as xyz:
+        xyz.write(f"{len(coordinates)}\n")
+        xyz.write(f"{output_file}\n")
+        for atom_data in coordinates:
+            atom, x, y, z = atom_data
+            xyz.write(f"{atom}       {x:.5f}       {y:.5f}       {z:.5f}\n")
+        xyz.write("END\n")
+    print(f"Successfully created '{output_file}'.")
+
+def extract_connectivity(pdb_file):
+    connectivity = {}
+    with open(pdb_file, 'r') as file:
+        conect = file.readlines()
+    for line in conect:
+        if "CONECT" in line:
+            c = line.split()
+            connectivity[c[1]] = c[2:]
+    return connectivity
+
+def check_length_connectivity(atom1, atom2, connectivity):
+    return atom2 in connectivity.get(atom1, [])
+
+def check_angle_connectivity(atom2, atom1, atom3, connectivity):
+    return atom1 in connectivity.get(atom2, []) and atom3 in connectivity.get(atom2, [])
+
+def bond_length(atom1, atom2, coordinates):
+    x1, y1, z1 = map(float, coordinates[atom1 - 1][1:])
+    x2, y2, z2 = map(float, coordinates[atom2 - 1][1:])
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+
+def bond_angle(atom2, atom1, atom3, coordinates):
+    x1, y1, z1 = map(float, coordinates[atom1 - 1][1:])
+    x2, y2, z2 = map(float, coordinates[atom2 - 1][1:])
+    x3, y3, z3 = map(float, coordinates[atom3 - 1][1:])
+    vec21 = (x1 - x2, y1 - y2, z1 - z2)
+    vec23 = (x3 - x2, y3 - y2, z3 - z2)
+    dot_product = vec21[0]*vec23[0] + vec21[1]*vec23[1] + vec21[2]*vec23[2]
+    mag21 = math.sqrt(vec21[0]**2 + vec21[1]**2 + vec21[2]**2)
+    mag23 = math.sqrt(vec23[0]**2 + vec23[1]**2 + vec23[2]**2)
+    cos_angle = dot_product / (mag21 * mag23)
+    return math.degrees(math.acos(cos_angle))
+
+def dihedral_angle(atom1, atom2, atom3, atom4, coordinates):
+    x1, y1, z1 = map(float, coordinates[atom1 - 1][1:])
+    x2, y2, z2 = map(float, coordinates[atom2 - 1][1:])
+    x3, y3, z3 = map(float, coordinates[atom3 - 1][1:])
+    x4, y4, z4 = map(float, coordinates[atom4 - 1][1:])
+    vec12 = (x2 - x1, y2 - y1, z2 - z1)
+    vec23 = (x3 - x2, y3 - y2, z3 - z2)
+    vec34 = (x4 - x3, y4 - y3, z4 - z3)
+    n123 = [vec12[1]*vec23[2] - vec12[2]*vec23[1], vec12[2]*vec23[0] - vec12[0]*vec23[2], vec12[0]*vec23[1] - vec12[1]*vec23[0]]
+    n234 = [vec23[1]*vec34[2] - vec23[2]*vec34[1], vec23[2]*vec34[0] - vec23[0]*vec34[2], vec23[0]*vec34[1] - vec23[1]*vec34[0]]
+    dot_pdt = n123[0]*n234[0] + n123[1]*n234[1] + n123[2]*n234[2]
+    mag123 = math.sqrt(n123[0]**2 + n123[1]**2 + n123[2]**2)
+    mag234 = math.sqrt(n234[0]**2 + n234[1]**2 + n234[2]**2)
+    cos_phi = dot_pdt / (mag123 * mag234)
+    return math.degrees(math.acos(cos_phi))
+
+file_path = input("Enter the file path: ").strip()
+if not os.path.exists(file_path):
+    print("ERROR IN INPUT! PLEASE ENTER A VALID FILE PATH..")
+    file_path = input("Enter the file path: ").strip()
+coordinates = extract_coordinates(file_path)
+print(len(coordinates))
+print(f"Coordinates of the conformer: {coordinates}")
+convert_to_xyz(file_path, coordinates)
+pdb_file = file_path.replace(".log", ".pdb")
+connectivity = extract_connectivity(pdb_file)
+print(f"Connectivity of the conformer: {connectivity}")
+answer = int(input("Enter 1 to continue and find the properties or 0 to exit: "))
+while answer == 1:
+    property = int(input("Enter 1 for finding bond length, 2 for bond angle, 3 for dihedral angle and 4 to compare between two conformers: "))
+    if property == 1: #bond length
+        atom1, atom2 = int(input("Enter the first atom: ")), int(input("Enter the second atom: "))
+        if atom1 > len(coordinates) or atom2 > len(coordinates):
+            print(f"ERROR IN INPUT! Atom {atom1} or atom {atom2} or both not in the conformer. PLEASE TRY AGAIN..")
+        elif check_length_connectivity(str(atom1), str(atom2), connectivity):
+            bl = bond_length(atom1, atom2, coordinates)
+            print(f"The bond length between {coordinates[atom1 - 1][0]}[{atom1}] and {coordinates[atom2 - 1][0]}[{atom2}] is {bl:.5f} Å.")
+        else:
+            print(f"{coordinates[atom1 - 1][0]}[{atom1}] and {coordinates[atom2 - 2][0]}[{atom2}] are not connected.")
+    elif property == 2: #bond angle
+        atom1, atom2, atom3 = int(input("Enter the first atom: ")), int(input("Enter the second atom: ")), int(input("Enter the third atom: "))
+        if atom1 > len(coordinates) or atom2 > len(coordinates) or atom3 > len(coordinates):
+            print(f"ERROR IN INPUT! Atom {atom1} or atom {atom2} or atom {atom3} or a few of them not in the conformer. PLEASE TRY AGAIN..")
+        elif check_angle_connectivity(str(atom2), str(atom1), str(atom3), connectivity):
+            ba = bond_angle(atom2, atom1, atom3, coordinates)
+            print(f"The bond angle formed by {coordinates[atom1 - 1][0]}[{atom1}], {coordinates[atom2 - 1][0]}[{atom2}] and {coordinates[atom3 - 1][0]}[{atom3}] is {ba:.5f} °.")
+        else:
+            print(f"{coordinates[atom1 - 1][0]}[{atom1}] and {coordinates[atom3 - 1][0]}[{atom2}] are not connected to {coordinates[atom2 - 1][0]}[{atom3}].")
+    elif property == 3: #dihedral angle
+        atom1, atom2, atom3, atom4 = int(input("Enter the first atom: ")), int(input("Enter the second atom: ")), int(input("Enter the third atom: ")), int(input("Enter the fourth atom: "))
+        if atom1 > len(coordinates) or atom2 > len(coordinates) or atom3 > len(coordinates) or atom4 > len(coordinates):
+            print(f"ERROR IN INPUT! Atom {atom1} or atom {atom2} or atom {atom3} or atom {atom4} or a few of them not in the conformer. PLEASE TRY AGAIN..")
+        else:
+            da = dihedral_angle(atom1, atom2, atom3, atom4, coordinates)
+            print(f"The dihedral angle formed by {coordinates[atom1 - 1][0]}[{atom1}], {coordinates[atom2 - 1][0]}[{atom2}], {coordinates[atom3 - 1][0]}[{atom3}] and {coordinates[atom4 - 1][0]}[{atom4}] is {da:.5f} °.")
+    elif property == 4: #comparison
+        file_path2 = input("Enter the file path: ").strip()
+        if not os.path.exists(file_path2):
+            print("ERROR IN INPUT! PLEASE ENTER A VALID FILE PATH..")
+            file_path2 = input("Enter the file path: ").strip()
+        elif file_path == file_path2:
+            print("YOU ENTERED THE SAME FILE PATH AS BEFORE!! PLEASE TRY AGAIN TO COMPARE..")
+            file_path2 = input("Enter the file path: ").strip()
+        coordinates2 = extract_coordinates(file_path)
+        print(f"COORDINATES OF THE CONFORMER: {coordinates}")
+        convert_to_xyz(file_path, coordinates)
+        pdb_file2 = file_path.replace(".log", ".pdb")
+        connectivity2 = extract_connectivity(pdb_file)
+        print(f"CONNECTIVITY DATA OF THE CONFORMER: {connectivity}")
+        property2 = int(input("Enter 1 for finding bond length, 2 for bond angle and 3 for dihedral angle: "))
+        if property2 == 1: #compare bond length
+            atom1, atom2 = int(input("Enter the first atom: ")), int(input("Enter the second atom: "))
+            if atom1 > len(coordinates2) or atom2 > len(coordinates2):
+                print(f"ERROR IN INPUT! Atom {atom1} or atom {atom2} or both not in the conformer. PLEASE TRY AGAIN..")
+            elif check_length_connectivity(str(atom1), str(atom2), connectivity2):
+                bl2 = bond_length(atom1, atom2, coordinates2)
+                print(f"The bond length between {coordinates2[atom1 - 1][0]}[{atom1}] and {coordinates2[atom2 - 1][0]}[{atom2}] is {bl2:.5f} Å.")
+            else:
+                print(f"{coordinates2[atom1 - 1][0]}[{atom1}] and {coordinates2[atom2 - 2][0]}[{atom2}] are not connected.")
+        elif property2 == 2: #compare bond angle
+            atom1, atom2, atom3 = int(input("Enter the first atom: ")), int(input("Enter the second atom: ")), int(input("Enter the third atom: "))
+            if atom1 > len(coordinates2) or atom2 > len(coordinates2) or atom3 > len(coordinates2):
+                print(f"ERROR IN INPUT! Atom {atom1} or atom {atom2} or atom {atom3} or a few of them not in the conformer. PLEASE TRY AGAIN..")
+            elif check_angle_connectivity(str(atom2), str(atom1), str(atom3), connectivity2):
+                ba2 = bond_angle(atom2, atom1, atom3, coordinates2)
+                print(f"The bond angle formed by {coordinates2[atom1 - 1][0]}[{atom1}], {coordinates2[atom2 - 1][0]}[{atom2}] and {coordinates2[atom3 - 1][0]}[{atom3}] is {ba2:.5f} °.")
+            else:
+                print(f"{coordinates[atom1 - 1][0]}[{atom1}] and {coordinates[atom3 - 1][0]}[{atom2}] are not connected to {coordinates[atom2 - 1][0]}[{atom3}].")
+        elif property2 == 3: #compare dihedral angle
+            atom1, atom2, atom3, atom4 = int(input("Enter the first atom: ")), int(input("Enter the second atom: ")), int(input("Enter the third atom: ")), int(input("Enter the fourth atom: "))
+            if atom1 > len(coordinates2) or atom2 > len(coordinates2) or atom3 > len(coordinates2) or atom4 > len(coordinates2):
+                print(f"ERROR IN INPUT! Atom {atom1} or atom {atom2} or atom {atom3} or atom {atom4} or a few of them not in the conformer. PLEASE TRY AGAIN..")
+            else:
+                da2 = dihedral_angle(atom1, atom2, atom3, atom4, coordinates2)
+                print(f"The dihedral angle formed by {coordinates2[atom1 - 1][0]}[{atom1}], {coordinates2[atom2 - 1][0]}[{atom2}], {coordinates2[atom3 - 1][0]}[{atom3}] and {coordinates2[atom4 - 1][0]}[{atom4}] is {da2:.5f} °.")
+        else:
+            print("ERROR IN INPUT! DO YOU WANT TO EXIT?")
+    else:
+        print("ERROR IN INPUT! DO YOU WANT TO EXIT??")
+    answer = int(input("Enter 1 to continue and find the properties or 0 to exit: "))
+print("Thankyou for using this tool! Have a nice day..")
