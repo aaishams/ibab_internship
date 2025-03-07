64854092e3b5bced36ac9f6600523f9855806e17
aaishams <115831912+aaishams@users.noreply.github.com>
2025-03-07 12:21:26 +0530

    Add files via upload

--- orbitals.py
+++ orbitals.py
@@ -22,29 +22,40 @@ def orbital_type(output_file):
     with open(output_file, 'r') as input:
         content = input.readlines()
     orbitals = {}
+    current_bond = ""
     for line in content:
-        if "LP" in line:
-            n = line.split()
-            orbitals[f"{n[0]} {n[5]}[{n[6]}]"] = f"1 n orbital"
-        if "BD ( 1)" in line:
-            s = line.split()
-            orbitals[f"{s[0]} {s[5]}[{s[6].replace("-", "")}]-{s[7]}[{s[8]}]"] = f"1 σ orbital"
-        if "BD ( 2)" in line:
-            sp = line.split()
-            orbitals[f"{sp[0]} {sp[5]}[{sp[6].replace("-", "")}]-{sp[7]}[{sp[8]}]"] = f"1 π orbital"
-        if "BD ( 3)" in line:
-            sp2 = line.split()
-            orbitals[f"{sp2[0]} {sp2[5]}[{sp2[6].replace("-", "")}]-{sp2[7]}[{sp2[8]}]"] = f"1 π orbital"
-        if "BD* ( 1)" in line:
-            ss = line.split()
-            orbitals[f"{ss[0]} {ss[5]}[{ss[6].replace("-", "")}]-{ss[7]}[{ss[8]}]"] = f"1 σ* orbital"
-        if "BD* ( 2)" in line:
-            sps = line.split()
-            orbitals[f"{sps[0]} {sps[5]}[{sps[6].replace("-", "")}]-{sps[7]}[{sps[8]}]"] = f"1 π* orbital"
-        if "BD* ( 3)" in line:
-            sp2s = line.split()
-            orbitals[f"{sp2s[0]} {sp2s[5]}[{sp2s[6].replace("-", "")}]-{sp2s[7]}[{sp2s[8]}]"] = f"1 π* orbital"
-    return orbitals
+        if "BD" in line or "BD*" in line or "LP" in line:
+            data = line.split()
+            bond_type = data[2]
+            if "LP" in line:
+                orbitals[f"{data[0]}"] = "n"
+            elif "BD " in line:
+                bond_key = f"{data[0]} {data[5]}[{data[6].replace("-", "")}]-{data[7]}[{data[8]}]"
+                current_bond = bond_key
+                orbitals[current_bond] = bond_type
+            elif "BD*" in line:
+                bond_key = f"{data[0]} {data[4]}[{data[5].replace("-", "")}]-{data[6]}[{data[7]}]"
+                current_bond = bond_key
+                orbitals[current_bond] = bond_type
+        elif current_bond and "p" in line:
+            values = line.split()
+            for item in values:
+                if "%)d" in item:
+                    p_contribution = float(item.split("%)d")[0])
+            if "BD*" in orbitals[current_bond]:
+                if p_contribution > 95:
+                    orbitals[current_bond] = "π*"
+                elif p_contribution < 95:
+                    orbitals[current_bond] = "σ*"
+            elif "BD" in orbitals[current_bond]:
+                if p_contribution > 95:
+                    orbitals[current_bond] = "π"
+                elif p_contribution < 95:
+                    orbitals[current_bond] = "σ"
+    modified_orbitals = {}
+    for k, v in orbitals.items():
+        modified_orbitals[k.split()[0]] = v
+    return modified_orbitals
 
 def extract_energies(file_path, output_file):
     with open(file_path, 'r') as file:
@@ -67,6 +78,29 @@ def extract_energies(file_path, output_file):
             with open(output_file, 'a') as out_file:
                 out_file.writelines(line)
 
+def interaction_energies(input_file, output_file, orbitals):
+    with open(input_file, 'r') as infile:
+        content = infile.readlines()
+    with open(output_file, 'w'):
+        pass
+    start_pattern = "within unit  1"
+    inside_section = False
+    for line in content:
+        if start_pattern in line:
+            inside_section = True
+            continue
+        if inside_section:
+            data = line.split()
+            orbitals1 = orbitals
+            for k, v in orbitals.items():
+                for k1, v1, in orbitals1.items():
+                    if "LP" in line and k == data[0] and k1 == data[6]:
+                        with open(output_file, 'a', encoding = "utf-8") as outfile:
+                            outfile.writelines(f"{k} {v} ({data[4]}[{data[5]}]) -> {k1} {v1} ({data[9]}[{data[10].replace("-", "")}]-{data[11]}[{data[12]}]) => {data[13]} \n")
+                    if "BD" in line and k == data[0] and k1 == data[8]:
+                        with open(output_file, 'a', encoding = "utf-8") as outfile:
+                            outfile.writelines(f"{k} {v} ({data[4]}[{data[5].replace("-", "")}]-{data[6]}[{data[7]}]) -> {k1} {v1} ({data[11]}[{data[12].replace("-", "")}]-{data[13]}[{data[14]}]) => {data[15]} \n")
+                
 file_path = input("Enter the log file path: ").strip()
 if not os.path.exists(file_path):
     print("Please enter a valid file path!")
@@ -83,5 +117,12 @@ output_file2 = "pe_output_" + os.path.basename(file_path.replace(".log", ".txt")
 extract_energies(file_path, output_file2)
 if os.path.exists(output_file2) and os.path.getsize(output_file2) != 0:
     print(f"Successfully saved {output_file2}")
+else:
+    print("Error in extraction! Please try again...")
+output_file3 = "ie_output_" + os.path.basename(file_path.replace(".log", ".txt"))
+
+interaction_energies(output_file2, output_file3, orbitals)
+if os.path.exists(output_file3) and os.path.getsize(output_file3) != 0:
+    print(f"Successfully saved {output_file3}")
 else:
     print("Error in extraction! Please try again...")
