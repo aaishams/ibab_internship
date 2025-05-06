def1527d2a9b747a08d4fd19df55fcace98874d2
aaishams <115831912+aaishams@users.noreply.github.com>
2025-05-06 17:14:22 +0530

    Add files via upload

---
+++ tw_1by2.r
@@ -0,0 +1,13 @@
+# Assuming
+L <- 1
+
+# X-axis and Y-axis values
+x_values <- seq(0, L, length.out = 1000)
+u_values <- (-1) * (sin(pi * x_values / L) + sin(2 * pi * x_values / L))
+
+# Final plot
+plot(x_values, u_values, type = "l", lwd = 2, xlim = c(0, L), ylim = c(-2, 2), 
+     xlab = "x", ylab = "u", main = "Travelling Wave no phase difference")
+
+# Label
+legend("bottomleft", legend = "t = 1/2𝜈")
