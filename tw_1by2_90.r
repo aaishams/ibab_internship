1b0594188b1059ba9ebc50d2c326a2db83f1db43
aaishams <115831912+aaishams@users.noreply.github.com>
2025-05-06 16:11:03 +0530

    Add files via upload

---
+++ tw_1by2_90.r
@@ -0,0 +1,12 @@
+# Assuming
+L <- 1
+
+# X-axis and Y-axis values
+x_values <- seq(0, L, length.out = 1000)
+u_values <- (- sin(pi * x_values / L))
+
+# Final plot
+plot(x_values, u_values, type = "l", lwd = 2, xlim = c(0, L), ylim = c(-2, 2), 
+     xlab = "x", ylab = "u", main = "Travelling Wave 90° phase difference")
+# Label
+legend("bottomleft", legend = "t = 1/2𝜈")
