e3a0840eafb2206ac4b2851977bdbb7e0104b9dd
aaishams <115831912+aaishams@users.noreply.github.com>
2025-05-28 19:54:59 +0530

    Add files via upload

---
+++ wave_function_probability_density.R
@@ -0,0 +1,14 @@
+n <- 2
+L <- 1
+x_values <- seq(0, L, length.out = 1000)
+psi_values <- c(sqrt(2 / L) * sin(n * pi * x_values / L))
+psi_square_values <- c((2 / L) * (sin(n * pi * x_values / L)) ^ 2)
+
+plot(x_values, psi_values, type = "l", ylim = c(0, 2), 
+     xlab = "x", ylab = "Function", main = "Plot for n = 1")
+lines(x_values, psi_square_values, col = "blue")
+
+legend(NULL, legend = legend("topright", 
+                             legend = c(expression(psi(x)), 
+                                        expression("|" * psi(x) * "|"^2)), 
+                             col = c("black", "blue"), lty = 1))
